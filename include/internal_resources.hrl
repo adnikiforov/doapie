cc656e31f2411a701a8691ff59e812723b093a6d
Andrey Nikiforov <a.d.nikiforov@gmail.com>
2014-12-05 11:20:07 +0300

    Added internal resources header

---
+++ include/internal_resources.hrl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author adnikiforov@gmail.com
+%%% @copyright (C) 2014
+%%% @doc
+%%% Internal resources
+%%% @end
+%%% Created : 5. Dec 2014 0:40
+%%%-------------------------------------------------------------------
+-author("a.d.nikiforov@gmail.com").
+
+-define(HEADER_CONTENT_TYPE, {"Content-Type", "application/json"}).
+-define(HEADER_AUTH, "Authorization").
+-define(HEADER_AUTH_BEARER, "Bearer ").
