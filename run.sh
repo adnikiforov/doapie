f14ee6936400016a92e771af2c6b0c218aca3ad4
Andrey Nikiforov <a.d.nikiforov@gmail.com>
2014-12-05 11:00:10 +0300

    Startup&config changes

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 !#/bin/sh
-rebar compile && erl -pa ebin -pa deps/*/ebin -config doapie.config -eval 'application:ensure_all_started(doapie).'
+rebar compile && erl -pa ebin -pa deps/*/ebin -config priv/doapie.config -eval 'application:ensure_all_started(doapie).'
